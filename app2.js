9897c13e07c77fee503fa7ef99640c1f35baa2b7
adsonb87 <adsonb87@gmail.com>
2019-06-21 13:59:48 -0300

    Detalhe

--- app2.js
+++ app2.js
@@ -37,8 +37,60 @@ fs.readFile('./RM181018.txt', 'UTF8' ,function(err,data){
 	console.log(linhas[0].toString().substring(94,100));
 	console.log(linhas[0].toString().substring(100,394));
 	console.log(linhas[0].toString().substring(394,400));*/
-	
-	
+
+	//Detalhe
+	/*for(var i=1 ; i<linhas.length; i++){
+		if(linhas[i].toString().substring(0,1) != 9){
+			console.log(linhas[i].toString().substring(0,1));
+			console.log(linhas[i].toString().substring(1,3));
+			console.log(linhas[i].toString().substring(3,17));
+			console.log(linhas[i].toString().substring(17,21));
+			console.log(linhas[i].toString().substring(21,23));
+			console.log(linhas[i].toString().substring(23,28));
+			console.log(linhas[i].toString().substring(28,29));
+			console.log(linhas[i].toString().substring(29,33));
+			console.log(linhas[i].toString().substring(33,37));
+			console.log(linhas[i].toString().substring(37,62));
+			console.log(linhas[i].toString().substring(62,70));
+			console.log(linhas[i].toString().substring(71,83));
+			console.log(linhas[i].toString().substring(83,86));
+			console.log(linhas[i].toString().substring(86,107));
+			console.log(linhas[i].toString().substring(107,108));
+			console.log(linhas[i].toString().substring(108,110));
+			console.log(linhas[i].toString().substring(110,120));
+			console.log(linhas[i].toString().substring(120,126));
+			console.log(linhas[i].toString().substring(126,139));
+			console.log(linhas[i].toString().substring(139,142));
+			console.log(linhas[i].toString().substring(142,147));
+			console.log(linhas[i].toString().substring(147,149));
+			console.log(linhas[i].toString().substring(149,150));
+			console.log(linhas[i].toString().substring(150,156));
+			console.log(linhas[i].toString().substring(156,158));
+			console.log(linhas[i].toString().substring(158,160));
+			console.log(linhas[i].toString().substring(160,173));
+			console.log(linhas[i].toString().substring(173,179));
+			console.log(linhas[i].toString().substring(179,192));
+			console.log(linhas[i].toString().substring(192,205));
+			console.log(linhas[i].toString().substring(205,218));
+			console.log(linhas[i].toString().substring(218,220));
+			console.log(linhas[i].toString().substring(220,234));
+			console.log(linhas[i].toString().substring(234,264));
+			console.log(linhas[i].toString().substring(264,274));
+			console.log(linhas[i].toString().substring(274,314));
+			console.log(linhas[i].toString().substring(314,326));
+			console.log(linhas[i].toString().substring(326,334));
+			console.log(linhas[i].toString().substring(334,349));
+			console.log(linhas[i].toString().substring(349,351));
+			console.log(linhas[i].toString().substring(351,381));
+			console.log(linhas[i].toString().substring(381,385));
+			console.log(linhas[i].toString().substring(385,391));
+			console.log(linhas[i].toString().substring(391,393));
+			console.log(linhas[i].toString().substring(393,394));
+			console.log(linhas[i].toString().substring(394,400));
+		}
+	}*/
+
+
 	/*linhas.forEach(function(linha){
 		var pos = linha.toString().substring(73,98);
 		console.log(pos);
