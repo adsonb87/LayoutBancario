a0651da02e1c155918b23c1adbf161ff5efe2204
adsonb87 <adsonb87@gmail.com>
2019-06-25 22:10:19 -0300

    Controlador Remessa

--- app/routes/layout.js
+++ app/routes/layout.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
 
 	app.get('/layout',function(req,res){
-		app.app.controllers.Trailer.remessa(app,req,res);
+		app.app.controllers.Remessa.remessa(app,req,res);
 	});
 };
