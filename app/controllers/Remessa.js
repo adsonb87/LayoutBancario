7d3ce3af82236fc735ba767c8d13fb0290e7d182
adsonb87 <adsonb87@gmail.com>
2019-06-29 12:01:35 -0300

    Att

--- app/controllers/Remessa.js
+++ app/controllers/Remessa.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 module.exports.remessa = function (app,req,res) {
 	var linhas;
 
-	fs.readFile('./app/files/RM181018.txt', 'UTF8' ,function(err,data){
+	fs.readFile('./app/files/RM181018 - Copia.txt', 'UTF8' ,function(err,data){
 		if(err){
 			console.error("Not Open: %s",err);
 			process.exit(1);
